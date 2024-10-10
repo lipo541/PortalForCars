ba0b8038e39ce30f402c257a01d7e1fef56c699e
lipo541 <141038060+lipo541@users.noreply.github.com>
2024-10-10 15:11:21 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
-import "./components/header/header";
-import Header from "./components/header/header";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <div>my name is zaza</div>
     </div>
   );
